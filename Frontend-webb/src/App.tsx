01ecc04fc6031ab7fdaf15c67807bbb563abfae0
Asplund-chasacademy <andreas.asplund@chasacademy.se>
2025-09-15 11:33:40 +0200

    Added Redux and login button

--- Frontend-webb/src/App.tsx
+++ Frontend-webb/src/App.tsx
@@ -1,5 +1,5 @@
 import { RouterProvider } from "react-router-dom";
-import router from "./routes"; // index.jsx i routes-mappen
+import router from "./routes"; // index.tsx i routes-mappen
 
 export default function App() {
   return <RouterProvider router={router} />;
--- Frontend-webb/src/main.tsx
+++ Frontend-webb/src/main.tsx
@@ -1,6 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
-import router from "./routes";   // eller "./routes/index"
 
-export default function App() {
-  return <RouterProvider router={router} />;
-}
+import { store } from "./store";     // <- import från nya store.ts
+import router from "./routes";       // din router-fil
+
+const rootEl = document.getElementById("root") as HTMLElement;
+
+ReactDOM.createRoot(rootEl).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
